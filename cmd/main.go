f620da21c66413a48d86c16536a89ceec567f21c
Mutsumi <chenyx030922@gmail.com>
2025-06-07 17:07:19 +0800

    add command line parse

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strings"
+)
 
+func main() {
+	id := flag.String("id", "", "ID of the Raft node (required)")
+	peersList := flag.String("peers", "", "Comma-separated list of peer addresses (required)")
+
+	flag.Parse()
+
+	if *id == "" {
+		log.Fatal("Error: -id flag is required")
+	}
+
+	if *peersList == "" {
+		log.Fatal("Error: -peers flag is required")
+	}
+
+	peers := strings.Split(*peersList, ",")
+	if len(peers) == 0 {
+		log.Fatal("Error: -peers list cannot be empty")
+	}
+
+	fmt.Printf("Node ID: %s\n", *id)
+	fmt.Printf("Peer Addresses: %v\n", peers)
+
+	// Placeholder for Raft node initialization and starting
+	// raftNode := raft.NewRaftNode(*id, peers, ...) // Adjust parameters as needed
+	// raftNode.Start()
+
+	// Keep the main function running (e.g., by listening on a channel or a signal)
+	// select {}
 }
